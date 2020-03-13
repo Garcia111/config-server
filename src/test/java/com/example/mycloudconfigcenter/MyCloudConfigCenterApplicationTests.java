a2fc90bace6a0984b51d08d088a83d84e8a3d4f9
chengyanxiao <chengyanxiao@ctsi.com.cn>
2020-03-13 22:58:47 +0800

    config-server初始提交

---
+++ src/test/java/com/example/mycloudconfigcenter/MyCloudConfigCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycloudconfigcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCloudConfigCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
